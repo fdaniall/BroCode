3ace5a48d2659ebdb77e05e0569d7df7f0e0dbc6
Fakhri Danial <fakhridanial29@gmail.com>
2023-10-01 21:25:31 +0700

    Java Tutorial

---
+++ Java/VariablesDataTypes.java
@@ -0,0 +1,13 @@
+public class VariablesDataTypes {
+
+	public static void main(String[] args) {
+		
+		int x = 123;
+		double y = 3.14;
+		boolean z = true;
+		char symbol = '@';
+		String name = "alpha";
+		
+		System.out.println(name);
+	}
+}
